ad209dc83da5849ee986f27234ca0a2d239159d3
joelmaykon <joelmaykon94@gmail.com>
2023-07-15 22:05:39 -0300

    feat: add network use loop

--- devops/terraform/azure/main.tf
+++ devops/terraform/azure/main.tf
@@ -7,7 +7,7 @@ provider "azurerm" {
 variable "location" {
   type        = string
   description = "Locale to resources of Azure."
-  default     = brazilsouth
+  default     = "westus"
 }
 
 variable "tags" {
@@ -41,4 +41,41 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.apex_group.name
   location            = var.location
   address_space       = var.vnet_address
+}
+
+resource "azurerm_resource_group" "vnet_resource_group" {
+  location = "westus"
+  name     = "vnet_resource_group"
+}
+
+resource "azurerm_network_security_group" "net_sec_group" {
+  name                = "apex"
+  location            = "westus"
+  resource_group_name = "vnet_resource_group"
+}
+
+variable "rules_input" {
+  type        = map(any)
+  description = "list ports open to web"
+  default = {
+    101 = 80
+    102 = 443
+    104 = 22
+  }
+}
+
+resource "azurerm_network_security_rule" "rules_input_on" {
+  network_security_group_name = "apex_net_sec_group"
+  for_each                    = var.rules_input
+  resource_group_name         = "vnet_resource_group"
+  name                        = "port_input_${each.value}"
+  priority                    = each.key
+  destination_port_range      = each.value
+  direction                   = "Inbound"
+  access                      = "Allow"
+  source_port_range           = "*"
+  source_address_prefix       = "*"
+  protocol                    = "Tcp"
+  destination_address_prefix  = "*"
+
 }
