759627d669ac9b704b128fbe573593832eefad64
joelmaykon <joelmaykon94@gmail.com>
2023-07-15 21:35:10 -0300

    feat: add variables terraform

--- devops/terraform/azure/main.tf
+++ devops/terraform/azure/main.tf
@@ -1,10 +1,44 @@
 provider "azurerm" {
   features {
-    
+
   }
 }
 
+variable "location" {
+  type        = string
+  description = "Locale to resources of Azure."
+  default     = brazilsouth
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Tags of resources and services azure"
+  default = {
+    environment = "dev"
+    manager     = "Joel Maykon"
+  }
+}
+
+variable "name-rg" {
+  type        = string
+  description = "Name of resource"
+  default     = "apex"
+}
+
+variable "vnet_address" {
+  type    = list(any)
+  default = ["10.0.0.0/16", "192.168.0.0/16"]
+}
+
 resource "azurerm_resource_group" "apex_group" {
-  name     = "apex"
-  location = "West Europe"
+  name     = var.name-rg
+  location = var.location
+  tags     = merge(var.tags, { version = "v2.1" })
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-terraform"
+  resource_group_name = azurerm_resource_group.apex_group.name
+  location            = var.location
+  address_space       = var.vnet_address
 }
