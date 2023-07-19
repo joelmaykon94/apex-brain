1737f2c14d13dfeb8ddf0a2718cc228fac553290
joelmaykon <joelmaykon94@gmail.com>
2023-07-19 13:43:21 -0300

    feat: create new project angular

---
+++ frontend/angular/clean-arch/src/presentation/feature/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'presentation-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.sass']
+})
+export class HomeComponent {
+
+}
